d470fe2ab8051005911fb7af37faf123163416aa
Bratok3000 <Bratok3000@gmail.com>
2019-02-05 23:28:36 +0200

    just small improvements

--- FLX_homework_6/homework/js/task1.js
+++ FLX_homework_6/homework/js/task1.js
@@ -3,20 +3,19 @@ var a = +prompt("Insert value a", "1");
 var b = +prompt("Insert value b", "1");
 var c = +prompt("Insert value c", "1");
 
-if(isNaN(a) || isNaN(b) || isNaN(c)) {
-    alert("one or more numbers are invalid");
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    alert("Invalid input data");
 } else {
     var d = b * b - 4 * a * c;
 
-    if(d < 0) {
-        alert("equation has no roots - no solution");
-    } else if(d === 0) {
+    if (d < 0) {
+        alert("no solution");
+    } else if (d === 0) {
         var x = -b / (2 * a);
-        alert("it's has one root with value of " + parseFloat(x.toFixed(2)));
-    } else if(d > 0) {
+        alert(parseFloat(x.toFixed(2)));
+    } else if (d > 0) {
         var x1 = (-b + Math.sqrt(d)) / (2 * a);
         var x2 = (-b - Math.sqrt(d)) / (2 * a);
         alert("x1 = " + (+x1.toFixed(2)) + " " + "x2 = " + (+x2.toFixed(2)));
     }
 }
-
--- FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/homework/js/task2.js
@@ -3,10 +3,10 @@
 let amountOfMoney = parseFloat(prompt("Insert amount of money between 0 and 9999999", "0"));
 let discount = parseInt(prompt("Insert discount between 0 and 99", "0"));
 
-if ( isNaN(amountOfMoney) || amountOfMoney < 0 || amountOfMoney > 9999999
-|| isNaN(discount) || discount < 0 || discount > 9999999 ) {
+if (isNaN(amountOfMoney) || amountOfMoney < 0 || amountOfMoney > 9999999 ||
+    isNaN(discount) || discount < 0 || discount > 9999999) {
     alert("Invalid input data");
-    } else {
+} else {
     let saved = amountOfMoney / 100 * discount;
     let priceWithDiscount = amountOfMoney - saved;
 
