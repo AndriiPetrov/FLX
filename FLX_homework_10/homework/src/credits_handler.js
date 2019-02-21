cc48ad6c95030a4b8acc7c849a66bcac60378831
Bratok3000 <Bratok3000@gmail.com>
2019-02-21 17:57:29 +0200

    make the progress

--- FLX_homework_10/homework/src/credits_handler.js
+++ FLX_homework_10/homework/src/credits_handler.js
@@ -1,4 +1,27 @@
-/* Your code goes here */
+// Task 1
+function userCard(index) {
+    let card = function getCardOptions() {
+        return {
+            balance: 100,
+            transactionLimit: 100,
+            hisroryLogs: [],
+            key: index
+        }
+    }
+    function putCredits (credits) {
+        card.balance = card.balance + credits;
+    }
+    function takeCredits (credits) {
+        if(card.balance > credits && card.transactionLimit > credits) {
+            card.balance = card.balance - credits;
+        } else {
+            console.error('card balance or transactionLimit are less then credits you want to take');
+        }
+    }
+    function setTransactionLimit(credits) {
+        card.transactionLimit = credits;
+    }
+}
 
 // Task 2
 function UserAccount() {
