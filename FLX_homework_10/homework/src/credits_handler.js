c5a470ce415817c177ab12b1830e0c65831ab3ae
Bratok3000 <Bratok3000@gmail.com>
2019-02-21 19:38:27 +0200

    finished task1

--- FLX_homework_10/homework/src/credits_handler.js
+++ FLX_homework_10/homework/src/credits_handler.js
@@ -1,31 +1,53 @@
 // Task 1
 function userCard(index) {
   let tax = 0.05;
+
   let getCardInformation = {
     balance: 100,
     transactionLimit: 100,
     historyLogs: [],
     key: index
   }
+
   return {
     getCardOptions: function () {
       return getCardInformation;
     },
     putCredits: function (credits) {
       getCardInformation.balance = getCardInformation.balance + credits;
+      getCardInformation.historyLogs[length] = {
+        operationType: 'Received credits',
+        credits: credits,
+        operationTime: Date.now
+      }
     },
     takeCredits: function (credits) {
-      if (getCardInformation.balance > credits && getCardInformation.transactionLimit > credits) {
+      if (getCardInformation.balance >= credits && getCardInformation.transactionLimit >= credits) {
         getCardInformation.balance = getCardInformation.balance - credits;
       } else {
         console.error('card balance or transactionLimit are less then credits you want to take');
       }
+      getCardInformation.historyLogs[length] = {
+        operationType: 'Withdrawal of credits',
+        credits: credits,
+        operationTime: Date.now
+      }
     },
     setTransactionLimit: function (credits) {
       getCardInformation.transactionLimit = credits;
+      getCardInformation.historyLogs[length] = {
+        operationType: 'Transaction limit change',
+        credits: credits,
+        operationTime: Date.now
+      }
     },
     transferCredits: function (credits, recepientCard) {
       credits = parseInt(credits - credits * tax);
+      if(getCardInformation.balance >= credits && getCardInformation.transactionLimit >= credits) {
+        recepientCard.getCardInformation.balance += credits; 
+      } else {
+        console.error('card balance or transactionLimit are less then credits you want to take')
+      }
     }
   }
 }
