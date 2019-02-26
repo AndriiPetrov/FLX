960bf57e6e786ec470f676d881685207d1a1a3f7
Bratok3000 <Bratok3000@gmail.com>
2019-02-26 23:59:25 +0200

    homework 12 before 00:00

--- FLX_homework_12/homework/src/app.js
+++ FLX_homework_12/homework/src/app.js
@@ -6,4 +6,33 @@ const todoItems = [
 
 // Your code goes here
 
-rootNode.appendChild(/* Append your list item node*/);
+// rootNode.appendChild(/* Append your list item node*/);
+
+let h1 = document.createElement('h1');
+h1.innerHTML = 'Simple TODO applcation';
+h1.style.textAlign = 'center';
+h1.className = 'h1'
+rootNode.appendChild(h1);
+
+let addNewTaskButton = document.createElement('button');
+addNewTaskButton.innerHTML = 'Add new task'
+addNewTaskButton.className = 'addNewTaskButton'
+rootNode.appendChild(addNewTaskButton)
+
+// let inputElem = document.createElement('input');
+// inputElem.setAttribute('type', 'text');
+// inputElem.className = 'input'
+// rootNode.appendChild(inputElem);
+
+
+addNewTaskButton.addEventListener('click', addNewTask);
+
+function addNewTask(e) {
+    location.href = '#/add'
+
+    let inputElem = document.createElement('input');
+    inputElem.setAttribute('type', 'text');
+    inputElem.className = 'input'
+    rootNode.appendChild(inputElem);
+    
+} 
